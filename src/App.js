c69038fdb2c44cb79894ae1aedae1e11474ae7d6
amine-chatteli <amin.chatteli@gmail.com>
2020-06-30 17:33:14 +0200

    cart manipulations final(debugging, implementing local storage)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import Navbar from './Navbar';
 import './App.css';
 import Banner from './Banner';
-import ProductList from './ProductList'
+import ProductList from './ProductList';
 import Cart from './Cart'
-import products from './products.json'
-
+import products from './products.json';
+import ls from 'local-storage'
 
 
 
@@ -17,7 +17,7 @@ class App extends Component {
       items: [],
       cart: [],
       totalItems: 0,
-      totalPrice:0
+      totalPrice: 0
     }
     this.handleShowCart = this.handleShowCart.bind(this);
     this.handleCloseCart = this.handleCloseCart.bind(this);
@@ -25,18 +25,29 @@ class App extends Component {
     this.handleIncreaseQuantity = this.handleIncreaseQuantity.bind(this);
     this.handleDecreaseQuantity = this.handleDecreaseQuantity.bind(this);
     this.handleRemoveItem = this.handleRemoveItem.bind(this);
-    this.handleClearCart=this.handleClearCart.bind(this);
+    this.handleClearCart = this.handleClearCart.bind(this);
   }
 
   componentDidMount() {
-    let items = products.items.map((item) => {
-      let id = item.sys.id
-      let title = item.fields.title;
-      let price = item.fields.price;
-      let image = item.fields.image.fields.file.url
-      return { id, title, price, image, quantity: 0 }
-    });
-    this.setState({ items })
+    let items=ls.get('items');
+    let cart=ls.get('cart');
+    let totalItems=ls.get('totalItems');
+    let totalPrice=ls.get('totalPrice');
+    if(items&&cart&&totalItems&&totalPrice){
+      this.setState({cart,items,totalItems,totalPrice});
+    }else{
+       items = products.items.map((item) => {
+          let id = item.sys.id
+          let title = item.fields.title;
+          let price = item.fields.price;
+          let image = item.fields.image.fields.file.url
+          return { id, title, price, image, quantity: 0 }
+        });
+        this.setState({ items })
+    }
+     
+        
+   
   }
 
   handleShowCart() {
@@ -54,7 +65,10 @@ class App extends Component {
       cart,
       cartVisibility: true
     });
-      this.totalItems(cart)
+    ls.set('cart',cart)
+    ls.set('items',this.state.items)
+    this.totalItems(cart);
+   
   }
   handleIncreaseQuantity(id) {
     let cart = [...this.state.cart];
@@ -69,6 +83,7 @@ class App extends Component {
     } else {
       this.handleRemoveItem(id);
     }
+   
   }
   handleRemoveItem(id) {
     let cart = this.state.cart.filter(item => item.id !== id);
@@ -76,36 +91,48 @@ class App extends Component {
     let item = this.state.items[id]
     item.quantity = 0;
     this.setState({ cart, items });
-    this.totalItems(this.state.cart);
+    ls.set('items',items);
+    ls.set('cart',cart);
+    this.totalItems(cart);
+   
   }
-  totalItems(cart){
-    let totalItems=0;
-    let totalPrice=0;
-    cart.forEach(item=>{
-      totalItems+=item.quantity
-      totalPrice+=(item.price*item.quantity);
-     this.setState({totalItems,totalPrice})
-      
+  totalItems(cart) {
+    let totalItems = 0;
+    let totalPrice = 0;
+    cart.forEach(item => {
+      totalItems += item.quantity
+      totalPrice += (item.price * item.quantity);
     })
+    this.setState({ totalItems, totalPrice })
+    ls.set('totalItems',totalItems);
+    ls.set('totalPrice',totalPrice);
+    ls.set('cart',cart);
+   
+  }
+  handleClearCart() {
+    let items = [...this.state.items];
+    items.forEach(item => {
+      item.quantity = 0
+    })
+    this.setState({
+      items,
+      cart: [],
+      totalItems: 0,
+      totalPrice: 0
+    })
+    ls.set('totalItems',0);
+    ls.set('totalPrice',0);
+    ls.set('cart',[]);
+    ls.set('items',items);
+  
+  }
 
-  }
-  handleClearCart(){
-    let items=[...this.state.items];
-    items.forEach(item=>{
-      item.quantity=0
-    })
-    this.setState({items,
-      cart:[],
-      totalItems:0,
-      totalPrice:0
-    })
-  }
   render() {
     let { totalItems } = this.state
     let { cart } = this.state
     let { items } = this.state;
     let { cartVisibility } = this.state;
-    let {totalPrice}=this.state;
+    let { totalPrice } = this.state;
     return (
       <div className="App">
         <Navbar onShowCart={this.handleShowCart} totalItems={totalItems} />
@@ -113,8 +140,8 @@ class App extends Component {
         <ProductList items={items} onAddToCart={this.handleAddToCart} />
         <Cart cartVisibility={cartVisibility} onCloseCart={this.handleCloseCart}
           cart={cart} onIncreaseQuantity={this.handleIncreaseQuantity}
-          onDecreaseQuantity={this.handleDecreaseQuantity} onRemoveItem={this.handleRemoveItem} 
-          onClearCart={this.handleClearCart} totalPrice={totalPrice}/>
+          onDecreaseQuantity={this.handleDecreaseQuantity} onRemoveItem={this.handleRemoveItem}
+          onClearCart={this.handleClearCart} totalPrice={totalPrice} />
       </div>
     );
   }
