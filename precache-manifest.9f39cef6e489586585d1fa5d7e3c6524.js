9d55af436302440285c96e912c908136b387d3c0
amine-chatteli <amin.chatteli@gmail.com>
2020-06-30 18:00:19 +0200

    Updates

---
+++ precache-manifest.9f39cef6e489586585d1fa5d7e3c6524.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77e879320345251e8e10ccd5aa705de",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "49b8b93e8643c922999d",
+    "url": "/react-shopping-cart/static/css/2.3a18d4ee.chunk.css"
+  },
+  {
+    "revision": "de0191c08417e9bcd387",
+    "url": "/react-shopping-cart/static/css/main.6a80d489.chunk.css"
+  },
+  {
+    "revision": "49b8b93e8643c922999d",
+    "url": "/react-shopping-cart/static/js/2.1c477906.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-shopping-cart/static/js/2.1c477906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0191c08417e9bcd387",
+    "url": "/react-shopping-cart/static/js/main.9eb802bd.chunk.js"
+  },
+  {
+    "revision": "bac7c289d168fab2c97e",
+    "url": "/react-shopping-cart/static/js/runtime-main.1c73a014.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-shopping-cart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-shopping-cart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-shopping-cart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-shopping-cart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-shopping-cart/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bb83d2d76884d198e31230535552cdc8",
+    "url": "/react-shopping-cart/static/media/hero-bcg.bb83d2d7.jpeg"
+  },
+  {
+    "revision": "6ba322fd1ecaf1bcde8ddee162c6769e",
+    "url": "/react-shopping-cart/static/media/logo.6ba322fd.svg"
+  },
+  {
+    "revision": "23d0ef2fec40e345cf49e27288823579",
+    "url": "/react-shopping-cart/static/media/product-1.23d0ef2f.jpeg"
+  },
+  {
+    "revision": "03e5f8d5b3420d785d7b20d3696527f0",
+    "url": "/react-shopping-cart/static/media/product-2.03e5f8d5.jpeg"
+  },
+  {
+    "revision": "ca5d8437d09540136a9178c230b6b1cb",
+    "url": "/react-shopping-cart/static/media/product-3.ca5d8437.jpeg"
+  },
+  {
+    "revision": "6a3db9a750d62fe448cf5dad7223de37",
+    "url": "/react-shopping-cart/static/media/product-4.6a3db9a7.jpeg"
+  },
+  {
+    "revision": "feebecebb11102fe32d3e3d394bbcd8a",
+    "url": "/react-shopping-cart/static/media/product-5.feebeceb.jpeg"
+  },
+  {
+    "revision": "2bba37fdd9b8296921d6ed3167c02a6e",
+    "url": "/react-shopping-cart/static/media/product-6.2bba37fd.jpeg"
+  },
+  {
+    "revision": "39e5f207127fdde3c9035339a24fbf5d",
+    "url": "/react-shopping-cart/static/media/product-7.39e5f207.jpeg"
+  },
+  {
+    "revision": "ae2168b81e4ec297c576aaaf873d96be",
+    "url": "/react-shopping-cart/static/media/product-8.ae2168b8.jpeg"
+  }
+]);
